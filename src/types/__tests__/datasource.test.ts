55c7d89bcbdddf292f8f5f0e11fb7cb8c944ccec
AleDore <alessio85.dore@gmail.com>
2023-12-21 15:04:17 +0100

    Refactor DB Datasource

---
+++ src/types/__tests__/datasource.test.ts
@@ -0,0 +1,168 @@
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import {
+  CDCConnectionSourceType,
+  CDCDataSource,
+  CosmosDBType,
+  DataSource,
+  MongoDBType,
+  PostgreSQLDBType,
+  SelectAllConnectionSourceType,
+  SelectAllDataSource,
+} from "../datasource";
+import * as E from "fp-ts/Either";
+import * as B from "fp-ts/boolean";
+import { pipe } from "fp-ts/lib/function";
+import { errorsToReadableMessages } from "@pagopa/ts-commons/lib/reporters";
+
+describe("DataSource", () => {
+  const aProperDatasourceConfig = {
+    connectionString: "foo",
+    type: "DB",
+    dbType: "CosmosDB",
+    sourceType: "CDC",
+    props: {
+      collectionName: "collName",
+      dbName: "dbName",
+      leaseContainerName: "lease",
+    },
+  };
+  it("should decode a correct datasource config properly", () => {
+    pipe(
+      aProperDatasourceConfig,
+      DataSource.decode,
+      E.map((decoded) => expect(decoded).toEqual(aProperDatasourceConfig)),
+      E.mapLeft(() => fail("Cannot decode a correct Datasource")),
+    );
+  });
+
+  it("should fail while decoding an empty datasource config", () => {
+    pipe(
+      {},
+      DataSource.decode,
+      E.map(() => fail("An empty datasource config should not be decoded")),
+      E.mapLeft((errs) => expect(errs).toBeDefined()),
+    );
+  });
+
+  it("should fail while decoding an incorrect datasource config", () => {
+    pipe(
+      { ...aProperDatasourceConfig, type: "OTHER" },
+      DataSource.decode,
+      E.map(() => fail("An incorrect datasource config should not be decoded")),
+      E.mapLeft((errs) => expect(errs).toBeDefined()),
+    );
+  });
+});
+
+describe("CDCDatasource", () => {
+  const aCosmosCdcDatasourceConfig = {
+    dbType: "CosmosDB" as CosmosDBType,
+    sourceType: "CDC" as CDCConnectionSourceType,
+    props: {
+      collectionName: "collName" as NonEmptyString,
+      dbName: "dbName" as NonEmptyString,
+      leaseContainerName: "lease" as NonEmptyString,
+    },
+  };
+
+  const aMongoCdcDataSourceConfig = {
+    ...aCosmosCdcDatasourceConfig,
+    dbType: "MongoDB" as MongoDBType,
+  };
+
+  const aSelectAllDatasourceConfig = {
+    ...aCosmosCdcDatasourceConfig,
+    sourceType: "SELECT_ALL" as SelectAllConnectionSourceType,
+  };
+
+  it.each`
+    description                                                              | input                                                     | success
+    ${"should decode a correct Cosmos CDCDatasource config properly"}        | ${aCosmosCdcDatasourceConfig}                             | ${true}
+    ${"should decode a correct Mongo CDCDatasource config properly"}         | ${aMongoCdcDataSourceConfig}                              | ${true}
+    ${"should fail giving a DocumentDB Datasource config with Postgre type"} | ${{ ...aMongoCdcDataSourceConfig, dbType: "PostgreSQL" }} | ${false}
+    ${"should fail with SelectAll Datasource config"}                        | ${aSelectAllDatasourceConfig}                             | ${false}
+    ${"should fail with an empty Datasource config"}                         | ${{}}                                                     | ${false}
+  `("$description", ({ input, success }) => {
+    pipe(input, CDCDataSource.decode, (eitherDecoded) =>
+      pipe(
+        success,
+        B.foldW(
+          () =>
+            pipe(
+              eitherDecoded,
+              E.map(() =>
+                fail("An incorrect CDCDatasource config should not be decoded"),
+              ),
+              E.mapLeft((errs) => expect(errs).toBeDefined()),
+            ),
+          () =>
+            pipe(
+              eitherDecoded,
+              E.map((decoded) => expect(decoded).toEqual(input)),
+              E.mapLeft(() => fail("Cannot decode a correct CDCDatasource")),
+            ),
+        ),
+      ),
+    );
+  });
+});
+
+describe("SelectAllDatasource", () => {
+  const aCosmosSelectAllDatasourceConfig = {
+    dbType: "CosmosDB" as CosmosDBType,
+    sourceType: "SELECT_ALL" as SelectAllConnectionSourceType,
+    props: {
+      dbName: "dbName" as NonEmptyString,
+      pageSize: 100,
+      resourceName: "resourceName",
+    },
+  };
+
+  const aMongoSelectAllDataSourceConfig = {
+    ...aCosmosSelectAllDatasourceConfig,
+    dbType: "MongoDB" as MongoDBType,
+    props: {
+      ...aCosmosSelectAllDatasourceConfig.props,
+      limit: 1,
+    },
+  };
+
+  const aPostgreSelectAllDatasourceConfig = {
+    ...aMongoSelectAllDataSourceConfig,
+    dbType: "PostgreSQL" as PostgreSQLDBType,
+  };
+
+  it.each`
+    description                                                               | input                                | success
+    ${"should decode a correct Cosmos SelectAllDatasource config properly"}   | ${aCosmosSelectAllDatasourceConfig}  | ${true}
+    ${"should decode a correct Mongo SelectAllDatasource config properly"}    | ${aMongoSelectAllDataSourceConfig}   | ${true}
+    ${"should decode a correct Postgres SelectAllDatasource config properly"} | ${aPostgreSelectAllDatasourceConfig} | ${true}
+    ${"should fail with an empty Datasource config"}                          | ${{}}                                | ${false}
+  `("$description", ({ input, success }) => {
+    pipe(input, SelectAllDataSource.decode, (eitherDecoded) =>
+      pipe(
+        success,
+        B.foldW(
+          () =>
+            pipe(
+              eitherDecoded,
+              E.map(() =>
+                fail(
+                  "An incorrect SelectAllDatasource config should not be decoded",
+                ),
+              ),
+              E.mapLeft((errs) => expect(errs).toBeDefined()),
+            ),
+          () =>
+            pipe(
+              eitherDecoded,
+              E.map((decoded) => expect(decoded).toEqual(input)),
+              E.mapLeft(() =>
+                fail("Cannot decode a correct SelectAllDatasource"),
+              ),
+            ),
+        ),
+      ),
+    );
+  });
+});
--- src/types/datasource.ts
+++ src/types/datasource.ts
@@ -1,74 +1,203 @@
+import {
+  NonNegativeInteger,
+  WithinRangeInteger,
+} from "@pagopa/ts-commons/lib/numbers";
 import * as t from "io-ts";
-import { NonEmptyString } from "io-ts-types";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { IsoDateFromString } from "@pagopa/ts-commons/lib/dates";
 
-export const CosmosDBDataSourceType = t.literal("CosmosDB");
-export type CosmosDBDataSourceType = t.TypeOf<typeof CosmosDBDataSourceType>;
-export const MongoDBDataSourceType = t.literal("MongoDB");
-export type MongoDBDataSourceType = t.TypeOf<typeof MongoDBDataSourceType>;
-export const PostgreSQLDataSourceType = t.literal("PostgreSQL");
-export type PostgreSQLDataSourceType = t.TypeOf<
-  typeof PostgreSQLDataSourceType
+export const CosmosDBType = t.literal("CosmosDB");
+export type CosmosDBType = t.TypeOf<typeof CosmosDBType>;
+export const MongoDBType = t.literal("MongoDB");
+export type MongoDBType = t.TypeOf<typeof MongoDBType>;
+export const PostgreSQLDBType = t.literal("PostgreSQL");
+export type PostgreSQLDBType = t.TypeOf<typeof PostgreSQLDBType>;
+
+export const CDCConnectionSourceType = t.literal("CDC");
+export type CDCConnectionSourceType = t.TypeOf<typeof CDCConnectionSourceType>;
+export const SelectAllConnectionSourceType = t.literal("SELECT_ALL");
+export type SelectAllConnectionSourceType = t.TypeOf<
+  typeof SelectAllConnectionSourceType
 >;
-export const UriConnectionCommon = t.type({
-  uri: NonEmptyString,
-});
-export type UriConnectionCommon = t.TypeOf<typeof UriConnectionCommon>;
 
-export const DBConnectionCommon = t.type({
+export const DBDataSourceType = t.literal("DB");
+export type DBDataSourceType = t.TypeOf<typeof DBDataSourceType>;
+
+export const QueueDataSourceType = t.literal("QUEUE");
+export type QueueDataSourceType = t.TypeOf<typeof QueueDataSourceType>;
+
+export const DataSourceConnectionCommon = t.type({
+  connectionString: NonEmptyString,
+});
+export type DataSourceConnectionCommon = t.TypeOf<
+  typeof DataSourceConnectionCommon
+>;
+
+export const CommonDocumentDbCdcProps = t.intersection([
+  t.type({
+    collectionName: NonEmptyString,
+    dbName: NonEmptyString,
+    leaseContainerName: NonEmptyString,
+  }),
+  t.partial({
+    leaseContainerPrefix: NonEmptyString,
+  }),
+]);
+export type CommonDocumentDbCdcProps = t.TypeOf<
+  typeof CommonDocumentDbCdcProps
+>;
+
+export const CosmosCdcProps = t.intersection([
+  CommonDocumentDbCdcProps,
+  t.partial({
+    startingFrom: IsoDateFromString,
+  }),
+]);
+
+export type CosmosCdcProps = t.TypeOf<typeof CosmosCdcProps>;
+
+export const CommonRelationalDbCdcProps = t.type({
   dbName: NonEmptyString,
+  replicaSlotName: NonEmptyString,
+  subscriptionName: NonEmptyString,
+  tableName: NonEmptyString,
+});
+export type CommonRelationalDbCdcProps = t.TypeOf<
+  typeof CommonRelationalDbCdcProps
+>;
+
+export const CommonCDCDataSource = t.type({
+  sourceType: CDCConnectionSourceType,
+});
+export type CommonCDCDataSource = t.TypeOf<typeof CommonCDCDataSource>;
+
+export const CDCCosmosDataSource = t.exact(
+  t.intersection([
+    CommonCDCDataSource,
+    t.type({
+      dbType: CosmosDBType,
+      props: CosmosCdcProps,
+    }),
+  ]),
+);
+export type CDCCosmosDataSource = t.TypeOf<typeof CDCCosmosDataSource>;
+
+export const CDCMongoDataSource = t.exact(
+  t.intersection([
+    CommonCDCDataSource,
+    t.type({
+      dbType: MongoDBType,
+      props: CommonDocumentDbCdcProps,
+    }),
+  ]),
+);
+export type CDCMongoDataSource = t.TypeOf<typeof CDCMongoDataSource>;
+
+export const CDCPostgresDataSource = t.exact(
+  t.intersection([
+    CommonCDCDataSource,
+    t.type({
+      dbType: PostgreSQLDBType,
+      props: CommonRelationalDbCdcProps,
+    }),
+  ]),
+);
+export type CDCPostgresDataSource = t.TypeOf<typeof CDCPostgresDataSource>;
+
+export const CDCDataSource = t.union([
+  CDCCosmosDataSource,
+  CDCMongoDataSource,
+  CDCPostgresDataSource,
+]);
+export type CDCDataSource = t.TypeOf<typeof CDCDataSource>;
+
+export const CommonDbSelectAllProps = t.type({
+  dbName: NonEmptyString,
+  pageSize: WithinRangeInteger(1, 1000),
   resourceName: NonEmptyString,
 });
-export type DBConnectionCommon = t.TypeOf<typeof DBConnectionCommon>;
+export type CommonDbSelectAllProps = t.TypeOf<typeof CommonDbSelectAllProps>;
 
-export const CosmosDBConnectionCommon = t.intersection([
-  UriConnectionCommon,
-  DBConnectionCommon,
+export const CommonDbSelectAllWithLimitProps = t.intersection([
+  CommonDbSelectAllProps,
+  t.type({
+    limit: NonNegativeInteger,
+  }),
 ]);
-export type CosmosDBConnectionCommon = t.TypeOf<
-  typeof CosmosDBConnectionCommon
+export type CommonDbSelectAllWithLimitProps = t.TypeOf<
+  typeof CommonDbSelectAllWithLimitProps
 >;
 
-export const ChangeFeedCommon = t.type({
-  createLeaseContainerIfNotExists: t.boolean,
-  leaseContainerName: NonEmptyString,
-  leaseContainerPrefix: NonEmptyString,
+export const CommonSelectAllDataSource = t.type({
+  sourceType: SelectAllConnectionSourceType,
 });
-export type ChangeFeedCommon = t.TypeOf<typeof ChangeFeedCommon>;
+export type CommonSelectAllDataSource = t.TypeOf<
+  typeof CommonSelectAllDataSource
+>;
 
-export const MongoDBConnectionCommon = t.intersection([
-  UriConnectionCommon,
-  DBConnectionCommon,
+export const SelectAllCosmosDataSource = t.exact(
+  t.intersection([
+    CommonSelectAllDataSource,
+    t.type({
+      dbType: CosmosDBType,
+      props: CommonDbSelectAllProps,
+    }),
+  ]),
+);
+export type SelectAllCosmosDataSource = t.TypeOf<
+  typeof SelectAllCosmosDataSource
+>;
+
+export const SelectAllMongoDataSource = t.exact(
+  t.intersection([
+    CommonSelectAllDataSource,
+    t.type({
+      dbType: MongoDBType,
+      props: CommonDbSelectAllWithLimitProps,
+    }),
+  ]),
+);
+export type SelectAllMongoDataSource = t.TypeOf<
+  typeof SelectAllMongoDataSource
+>;
+
+export const SelectAllPostgresDataSource = t.exact(
+  t.intersection([
+    CommonSelectAllDataSource,
+    t.type({
+      dbType: PostgreSQLDBType,
+      props: CommonDbSelectAllWithLimitProps,
+    }),
+  ]),
+);
+export type SelectAllPostgresDataSource = t.TypeOf<
+  typeof SelectAllPostgresDataSource
+>;
+
+export const SelectAllDataSource = t.union([
+  SelectAllCosmosDataSource,
+  SelectAllMongoDataSource,
+  SelectAllPostgresDataSource,
 ]);
-export type MongoDBConnectionCommon = t.TypeOf<typeof MongoDBConnectionCommon>;
+export type SelectAllDataSource = t.TypeOf<typeof SelectAllDataSource>;
 
-export const CosmosDBConnectionChangeFeed = t.exact(
-  t.intersection([CosmosDBConnectionCommon, ChangeFeedCommon]),
-);
-export type CosmosDBConnectionChangeFeed = t.TypeOf<
-  typeof CosmosDBConnectionChangeFeed
->;
-
-export const MongoDBConnectionChangeFeed = t.exact(
-  t.intersection([MongoDBConnectionCommon, ChangeFeedCommon]),
-);
-export type MongoDBConnectionChangeFeed = t.TypeOf<
-  typeof MongoDBConnectionChangeFeed
->;
-
-export const CosmosDBDataSource = t.type({
-  connection: CosmosDBConnectionChangeFeed,
-  type: CosmosDBDataSourceType,
-});
-export type CosmosDBDataSource = t.TypeOf<typeof CosmosDBDataSource>;
-
-export const MongoDBDataSource = t.type({
-  connection: MongoDBConnectionChangeFeed,
-  type: MongoDBDataSourceType,
-});
-export type MongoDBDataSource = t.TypeOf<typeof MongoDBDataSource>;
-
-export const DBDataSource = t.union([
-  CosmosDBDataSource,
-  MongoDBConnectionChangeFeed,
+export const DBDataSource = t.intersection([
+  t.exact(
+    t.type({
+      type: DBDataSourceType,
+    }),
+  ),
+  t.exact(
+    t.partial({
+      delay: NonNegativeInteger,
+    }),
+  ),
+  t.union([CDCDataSource, SelectAllDataSource]),
 ]);
 export type DBDataSource = t.TypeOf<typeof DBDataSource>;
+
+export const DataSource = t.intersection([
+  DataSourceConnectionCommon,
+  DBDataSource,
+]);
+export type DataSource = t.TypeOf<typeof DataSource>;
